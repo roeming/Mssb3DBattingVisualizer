78343caae8d513f8b9d6ca8112515c966bf6f72d
Nicholas Lannutti <nlannutti97@gmail.com>
2023-10-21 19:42:30 -0400

    Connected most inputs to calculator

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ render_dimensions = (1280, 720)
 
 DEFAULT_STADIUM = "Stadiums/Mario Stadium.json"
 
+CHARACTERNAME_TO_ID = {
+    "Mario": 0,
+    "Luigi": 1
+}
+
+ID_TO_CHARACTERNAME = {v: k for k, v in CHARACTERNAME_TO_ID.items()}
+
+
 KEYBOARD_CAMERA_CONTROLS = {
     pygame.K_w      : lambda movement, delta_time, sensitivity : movement + (Vector3(0, 0, 1) * delta_time * sensitivity),
     pygame.K_s      : lambda movement, delta_time, sensitivity : movement - (Vector3(0, 0, 1) * delta_time * sensitivity),
@@ -19,6 +27,62 @@ KEYBOARD_CAMERA_CONTROLS = {
     pygame.K_a      : lambda movement, delta_time, sensitivity : movement + (Vector3(1, 0, 0) * delta_time * sensitivity),
     pygame.K_d      : lambda movement, delta_time, sensitivity : movement - (Vector3(1, 0, 0) * delta_time * sensitivity),
     pygame.K_LSHIFT : lambda movement, delta_time, sensitivity : movement * 2.0,
+}
+
+PARSE_GUI_INPUTS = {
+    "-BATTER-ID-"       : lambda GUI_Value : CHARACTERNAME_TO_ID[GUI_Value],
+    "-PITCHER-ID-"      : lambda GUI_Value : CHARACTERNAME_TO_ID[GUI_Value],
+    "-BATTER-ISLEFTY-"  : lambda GUI_Value : 1 if GUI_Value else 0,
+    #The next 3 check if value is a float before using the float function, then truncates to -2 to 2.
+    #The truncating should be made smarter in the future to use real values from the game.
+    "-BATTER-X-"        : lambda GUI_Value : 0 if not GUI_Value.replace(".","").replace("-","").isnumeric() else min(max(float(GUI_Value),-2),2),
+    "-BALL-X-"          : lambda GUI_Value : 0 if not GUI_Value.replace(".","").replace("-","").isnumeric() else min(max(float(GUI_Value),-2),2),
+    "-BALL-Z-"          : lambda GUI_Value : 0 if not GUI_Value.replace(".","").replace("-","").isnumeric() else min(max(float(GUI_Value),-2),2),
+    "-CHEM-LINKS-"      : lambda GUI_Value: GUI_Value,
+    "-HITTYPE-SLAP-"    : lambda GUI_Value: 0,
+    "-HITTYPE-CHARGE-"  : lambda GUI_Value: 1,
+    "-PITCHTYPE-CURVE-" : lambda GUI_Value: 0,
+    "-PITCHTYPE-CHARGE-": lambda GUI_Value: 1,
+    "-PITCHTYPE-PERFECT-" 
+                        : lambda GUI_Value: 2,
+    "-PITCHTYPE-CHANGEUP-"  
+                        : lambda GUI_Value: 3,
+    "-CHARGE-UP-"       : lambda GUI_Value: min(max(GUI_Value, 0), 1),
+    "-CHARGE-DOWN-"     : lambda GUI_Value: min(max(GUI_Value, 0), 1),
+    "-CONTACT-FRAME-"   : lambda GUI_Value: int(GUI_Value),
+    "-STICK-UP-"        : lambda GUI_Value: GUI_Value,
+    "-STICK-LEFT-"      : lambda GUI_Value: GUI_Value,
+    "-STICK-RIGHT-"     : lambda GUI_Value: GUI_Value,
+    "-STICK-DOWN-"      : lambda GUI_Value: GUI_Value,
+    "-RNG-1-"           : lambda GUI_Value: 0 if not GUI_Value.isnumeric() else min(max(int(GUI_Value),0),32767),
+    "-RNG-2-"           : lambda GUI_Value: 0 if not GUI_Value.isnumeric() else min(max(int(GUI_Value),0),32767),
+    "-RNG-3-"           : lambda GUI_Value: 0 if not GUI_Value.isnumeric() else min(max(int(GUI_Value),0),32767)
+}
+
+GUIName_TO_JSONName = {
+    "-BATTER-ID-"           : "batter_id",
+    "-PITCHER-ID-"          : "pitcher_id",
+    "-BATTER-ISLEFTY-"      : "handedness",
+    "-BATTER-X-"            : "batter_x",
+    "-BALL-X-"              : "ball_x",
+    "-BALL-Z-"              : "ball_z",
+    "-CHEM-LINKS-"          : "chem",
+    "-HITTYPE-SLAP-"        : "hit_type",
+    "-HITTYPE-CHARGE-"      : "hit_type",
+    "-PITCHTYPE-CURVE-"     : "pitch_type",
+    "-PITCHTYPE-CHARGE-"    : "pitch_type",
+    "-PITCHTYPE-PERFECT-"   : "pitch_type",
+    "-PITCHTYPE-CHANGEUP-"  : "pitch_type",
+    "-CHARGE-UP-"           : "charge_up",
+    "-CHARGE-DOWN-"         : "charge_down",
+    "-CONTACT-FRAME-"       : "frame",
+    "-STICK-UP-"            : "stick_up",
+    "-STICK-LEFT-"          : "stick_left",
+    "-STICK-RIGHT-"         : "stick_right",
+    "-STICK-DOWN-"          : "stick_down",
+    "-RNG-1-"               : "rand_1",
+    "-RNG-2-"               : "rand_2",
+    "-RNG-3-"               : "rand_3"
 }
 
 class RenderedBattingScene:
@@ -307,56 +371,121 @@ class RenderedBattingScene:
 
 class ParameterWindow:
     def __init__(self) -> None:
-        originalGUI_column = [
-            [sg.Multiline("{}", key="-BATTING-JSON-", enable_events=True, expand_y=True, auto_size_text=True)],
-            [sg.Button("Instructions", key="-INSTRUCTIONS-", enable_events=True), sg.Button("Show Resulting Hit Details", key="-SHOW-HIT-DETAILS-", enable_events=True)]
-        ]
+        #originalGUI_column = [
+        #    [sg.Multiline("{}", key="-BATTING-JSON-", enable_events=True, expand_y=True, auto_size_text=True)],
+        #    [sg.Button("Instructions", key="-INSTRUCTIONS-", enable_events=True), sg.Button("Show Resulting Hit Details", key="-SHOW-HIT-DETAILS-", enable_events=True)]
+        #]
         
-    
+                #set default inputs
+        self.input_params = {
+            "batter_id": 0,
+            "is_batter_captain": False,
+            "pitcher_id": 0,
+            "handedness": 0,
+            "batter_x": 0,
+            "ball_x": 0,
+            "ball_z": 0,
+            "chem": 0,
+            "hit_type": 0,
+            "pitch_type": 0,
+            "charge_up": 0,
+            "charge_down": 0,
+            "frame": 6,
+            "stick_up": False,
+            "stick_down": False,
+            "stick_left": False,
+            "stick_right": False,
+            "rand_1":1565,
+            "rand_2":20008,
+            "rand_3":1628
+        }
+
+        #create GUI and fill default values according to the input_params defaults
         visualizer_param_column = [
-            [sg.Text("Batter ID"), sg.Combo(values=('Mario', 'Luigi'), default_value="Mario"),  sg.Checkbox('Superstar', default=False), sg.Checkbox('Lefty', default=False)],
-            [sg.Text("Pitcher ID"), sg.Combo(values=('Mario', 'Luigi'), default_value="Mario"),  sg.Checkbox('Superstar', default=False), sg.Checkbox('Lefty', default=False)],
-            [sg.Text("Batter x"),sg.InputText(0)], 
-            [sg.Text("Ball x"),sg.InputText(0)], 
-            [sg.Text("Ball z"),sg.InputText(0)],
-            [sg.Text("Chemistry Links on Base"), sg.Combo(values=(0, 1, 2, 3), default_value=0)],
-            [sg.Text("Swing Type"), sg.Combo(values=('Slap', 'Charge'),default_value="Slap")],
-            [sg.Text("Pitch Type"), sg.Combo(values=('Curve', 'Charge', 'Perfect', 'Change Up'), default_value="Curve")],
-            [sg.Text("Charge Up"), sg.Slider(range=(0,1), default_value=0, orientation='horizontal', resolution=0.01, tick_interval=0.25)],
-            [sg.Text("Charge Down"), sg.Slider(range=(0,1), default_value=0, orientation='horizontal', resolution=0.01, tick_interval=0.25)],
-            [sg.Text("Contact Frame"), sg.Slider(range=(2,10), default_value=6, orientation='horizontal', resolution=1, tick_interval=1)],
-            [sg.Text("Stick Input"), sg.Checkbox("↑", default=False), sg.Checkbox("←", default=False), sg.Checkbox("→", default=False), sg.Checkbox("↓", default=False)],
-            [sg.Text("RNG 1"),sg.InputText(1565)], 
-            [sg.Text("RNG 2"),sg.InputText(20008)], 
-            [sg.Text("RNG 3"),sg.InputText(1628)],
-            [sg.Text("Vertical Range Override"),sg.InputText()], 
+            [
+                sg.Text("Batter ID"), 
+                sg.Combo(values=('Mario', 'Luigi'), default_value=ID_TO_CHARACTERNAME[self.input_params["batter_id"]], key="-BATTER-ID-", enable_events=True),  
+                sg.Checkbox('Superstar', default=False), 
+                sg.Checkbox('Lefty', default=self.input_params["handedness"], key="-BATTER-ISLEFTY-",enable_events=True)
+            ],
+
+            [
+                sg.Text("Pitcher ID"), 
+                sg.Combo(values=('Mario', 'Luigi'), default_value=ID_TO_CHARACTERNAME[self.input_params["pitcher_id"]], key="-PITCHER-ID-", enable_events=True),  
+                sg.Checkbox('Superstar', default=False), 
+                sg.Checkbox('Lefty', default=False)
+            ],
+
+            [sg.Text("Batter x"), sg.InputText(self.input_params["batter_x"], key="-BATTER-X-", enable_events=True)], 
+            [sg.Text("Ball x"), sg.InputText(self.input_params["ball_x"], key="-BALL-X-", enable_events=True)], 
+            [sg.Text("Ball z"), sg.InputText(self.input_params["ball_z"], key="-BALL-Z-", enable_events=True)],
+
+            [sg.Text("Chemistry Links on Base"), sg.Combo(values=(0, 1, 2, 3), default_value=self.input_params["chem"], key="-CHEM-LINKS-", enable_events=True)],
+
+            [sg.Text("Swing Type"), 
+                sg.Radio('Slap', group_id="swingTypes", default= False if self.input_params["hit_type"] else True, key="-HITTYPE-SLAP-", enable_events=True),  
+                sg.Radio('Charge', group_id="swingTypes", default= True if self.input_params["hit_type"] else False, key="-HITTYPE-CHARGE-", enable_events=True), ],
+
+            [sg.Text("Pitch Type"), 
+                sg.Radio('Curve', group_id="pitchTypes", key="-PITCHTYPE-CURVE-", default= True if self.input_params["pitch_type"] == 0 else False, enable_events=True), 
+                sg.Radio('Charge', group_id="pitchTypes", key="-PITCHTYPE-CHARGE-", default= True if self.input_params["pitch_type"] == 1 else False, enable_events=True), 
+                sg.Radio('Perfect', group_id="pitchTypes", key="-PITCHTYPE-PERFECT-", default= True if self.input_params["pitch_type"] == 2 else False, enable_events=True), 
+                sg.Radio('ChangeUp', group_id="pitchTypes", key="-PITCHTYPE-CHANGEUP-", default= True if self.input_params["pitch_type"] == 3 else False, enable_events=True), ],
+
+            [sg.Text("Charge Up"), sg.Slider(range=(0,1), default_value=self.input_params["charge_up"], orientation='horizontal', 
+                                             resolution=0.01, tick_interval=0.25, key="-CHARGE-UP-", enable_events=True)],
+            [sg.Text("Charge Down"), sg.Slider(range=(0,1), default_value=self.input_params["charge_down"], orientation='horizontal', 
+                                               resolution=0.01, tick_interval=0.25, key="-CHARGE-DOWN-", enable_events=True)],
+
+            [sg.Text("Contact Frame"), sg.Slider(range=(2,10), default_value=self.input_params["frame"], orientation='horizontal', 
+                                                 resolution=1, tick_interval=1, key="-CONTACT-FRAME-", enable_events=True)],
+
+            [
+                sg.Text("Stick Input"), 
+                sg.Checkbox("↑", default=False, key="-STICK-UP-", enable_events=True), 
+                sg.Checkbox("←", default=False, key="-STICK-LEFT-", enable_events=True), 
+                sg.Checkbox("→", default=False, key="-STICK-RIGHT-", enable_events=True), 
+                sg.Checkbox("↓", default=False, key="-STICK-DOWN-", enable_events=True)
+            ],
+
+            [sg.Text("RNG 1"),sg.InputText(self.input_params["rand_1"], key="-RNG-1-", enable_events=True)], 
+            [sg.Text("RNG 2"),sg.InputText(self.input_params["rand_2"], key="-RNG-2-", enable_events=True)], 
+            [sg.Text("RNG 3"),sg.InputText(self.input_params["rand_3"], key="-RNG-3-", enable_events=True)],
+
+            [sg.Text("Vertical Range Override"),sg.InputText()], #TODO: connect rest of default values with input-params
             [sg.Text("Vertical Angle Override"),sg.InputText()], 
             [sg.Text("Horizontal Angle Override"),sg.InputText()],  
             [sg.Text("Power Override"),sg.InputText()],  
+
             [sg.Checkbox("Show One Hit", default=False)],  
             [sg.Checkbox("Generate Random Hits", default=False)],   
+
             [sg.Checkbox("Show FPS", default=False)],  
-            [sg.Checkbox("Convert Units to Feet", default=False)],  
+            [sg.Checkbox("Convert Units to Feet", default=False)], 
+             
             [sg.Text("Stadium Path"),sg.InputText()] 
         ]
         
         layout = [
-            [sg.Column(originalGUI_column),
-            sg.Column(visualizer_param_column)]
+            [sg.Column(visualizer_param_column)]
         ]
 
         self.window = sg.Window("Render Parameters", layout, resizable=True, enable_close_attempted_event=True)
         self.parsed_input = {}
         self.instructions_text = ""
 
+
+
     def update_values(self):
         new_input = None
         json_updated = False
-        for event_values in self.window.read(timeout=1/1000):
-            if event_values == None or event_values == "__TIMEOUT__":
-                break
-
-            event = event_values
+        
+        event, values = self.window.read(timeout=1/1000)
+        #for event in self.window.read(timeout=1/1000):
+        if event == None or event == "__TIMEOUT__":
+            pass
+        else:
+            event = event
             if event == "-WINDOW CLOSE ATTEMPTED-":
                 exit()
             elif event == "-BATTING-JSON-":
@@ -378,8 +507,55 @@ class ParameterWindow:
                 except Exception as e:
                     pass
 
+
+            #parse changes to the GUI
+            if event == "-WINDOW CLOSE ATTEMPTED-":
+                exit()
+            else:
+                for key, func in PARSE_GUI_INPUTS.items():
+                    if event == key:
+                        test = GUIName_TO_JSONName[key]
+                        self.input_params[GUIName_TO_JSONName[key]] = func(values[event])
+                        test = self.input_params[GUIName_TO_JSONName[key]]
+
+
+            #elif event == "-BATTER-ID-":
+             #   self.input_params["batter_id"] = CHARACTERNAME_TO_ID[self.window["-BATTER-ID-"].get()]
+            #elif event == "-PITCHER-ID-":
+             #   self.input_params["pitcher_id"] = CHARACTERNAME_TO_ID[self.window["-PITCHER-ID-"].get()]
+            #elif event == "-BATTER-ISLEFTY-":
+                #sets handedness to lefty (1) if checkbox is checked.
+             #   self.input_params["handedness"] = 1 if self.window["-BATTER-ISLEFTY-"].get() else 0
+            #elif event == "-BATTER-X-":
+            #    try:
+            #        if abs(float(self.window["-BATTER-X-"].get())) < 2:
+            #            self.input_params["batter_x"] = float(self.window["-BATTER-X-"].get())
+            #        else:
+            #            self.input_params["batter_x"] = 0
+            #    except Exception as e:
+            #        pass
+            # elif event == "-BALL-X-":
+            #     try:
+            #         if abs(float(self.window["-BALL-X-"].get())) < 2:
+            #             self.input_params["ball_x"] = float(self.window["-BALL-X-"].get())
+            #         else:
+            #             self.input_params["ball_x"] = 0
+            #     except Exception as e:
+            #         pass
+            # elif event == "-BALL-Z-":
+            #     try:
+            #         if abs(float(self.window["-BALL-Z-"].get())) < 2:
+            #             self.input_params["ball_z"] = float(self.window["-BALL-Z-"].get())
+            #         else:
+            #             self.input_params["ball_z"] = 0
+            #     except Exception as e:
+            #         pass
+
+        #overriding this to true for while i'm porting over the logic.
+        json_updated = True
         if json_updated:
-            self.parsed_input = new_input
+            #self.parsed_input = new_input
+            self.parsed_input = self.input_params
 
         return json_updated
 
